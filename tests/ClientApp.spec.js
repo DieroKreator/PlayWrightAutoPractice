b9972729d914c6bf3d604406d0a8382264d532d6
DieroKreator <qtechwired@gmail.com>
2025-04-07 18:53:55 -0300

    Refactor Browser Context test to improve login functionality and update test structure

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,14 +1,18 @@
-const {test, expect} = require('playwright/test');
+const { test, expect } = require('@playwright/test');
+ 
 
-test.skip('Browser Context Playwright test', async ({page})=> { 
-    
-    await page.goto('https://rahulshettyacademy.com/client');
-    await page.locator("#userEmail").fill("anshika@gmail.com");
-    await page.locator("#userPassword").fill("Iamking@000");
-    await page.locator("[value='login']").click();    
-    // await page.waitForLoadState('networkidle');
-    await page.locator(".card-body b").first().waitFor();
-    const titles = await page.locator(".card-body b").allTextContents();
-
-    console.log(titles);
-});
+test('@Web Client App login', async ({ page }) => {
+   //js file- Login js, DashboardPage
+   const email = "anshika@gmail.com";
+   const productName = 'zara coat 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").type("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
+ 
+})
