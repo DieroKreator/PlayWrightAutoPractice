c4a97bec6a53dcd799af606b56e56a14cd685806
DieroKreator <qtechwired@gmail.com>
2025-09-10 13:01:24 -0300

    Implement dynamic order retrieval in Client App login test for improved order validation

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -57,4 +57,21 @@ test.only('Client App login', async ({ page }) => {
     expect(page.locator(".hero-primary").first()).toHaveText(" Thankyou for the order. ");
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
     console.log(orderId);
+
+    //Dinamically find an order
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+
+    const rows = await page.locator("tbody tr");
+
+    for (let i = 0; i < await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
 })
