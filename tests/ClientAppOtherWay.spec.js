ef5ba2950dac6061fa43b046d7687e4fce16ce3d
DieroKreator <qtechwired@gmail.com>
2025-09-11 20:07:20 -0300

    Added test with alt way to find elements and interact

---
+++ tests/ClientAppOtherWay.spec.js
@@ -0,0 +1,32 @@
+const { test, expect } = require('@playwright/test');
+const { assert } = require('console');
+
+
+test.only('Client App login', async ({ page }) => {
+    const email = "pepe1010@gmail.com";
+    const productName = "ZARA COAT 3";
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder('email@example.com').fill(email);
+    await page.getByPlaceholder('enter your passsword').fill("Iamking@000");
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+
+    await page.locator(".card-body").filter({ hasText:"ZARA COAT 3" })
+              .getByRole('button', { name: 'Add To Cart' }).click();
+
+    await page.getByRole('listitem').getByRole('button', { name: 'Cart' }).click();
+
+    page.locator("ul.cartWrap li.items").first().waitFor();
+    await page.getByText('ZARA COAT 3').isVisible();
+
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    await page.locator("input[placeholder*='Select Country']").pressSequentially('ind', { delay: 150 });
+
+    await page.getByRole("button",{name :"India"}).nth(1).click();
+    await page.getByText("PLACE ORDER").click();
+
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+})
