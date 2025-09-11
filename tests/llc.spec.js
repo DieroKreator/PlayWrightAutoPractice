1748fb02d34d49a70505e86158a3152cc09865b8
DieroKreator <qtechwired@gmail.com>
2025-09-11 16:13:17 -0300

    Refactor Playwright test to include form submission and product addition steps

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -5,5 +5,11 @@ test('Playwright Special locators', async ({ page }) => {
     await page.getByLabel('Check me out if you Love IceCreams!').click();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").select("Female");
+
+    await page.getByPlaceholder('Password').fill("123456");
+    await page.getByRole("button", { name: "Submit" }).click();
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link", { name: "Shop" }).click();
+    await page.locator("app-card").filter({ hasText: 'Nokia Edge' }).getByRole("button", { name: "Add" }).click();
     
 });
