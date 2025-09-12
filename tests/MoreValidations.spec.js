be017f4ab103693de83f66447735e0b9912060bf
DieroKreator <qtechwired@gmail.com>
2025-09-12 15:04:06 -0300

    Alert popups and Frames

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+test.only("Pop validations", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+    // await page.goto("http://google.com");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+    page.pause();
+    page.on('dialog', dialog => dialog.accept());
+    // page.on('dialog', dialog => dialog.dismiss());
+    await page.locator("#confirmbtn").click();
+    await page.locator("#mousehover").hover();
+
+    const framePage = page.frameLocator("#courses-iframe");
+    await framePage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framePage.locator(".text h2").textContent();
+    textCheck.split(" ")[1];
+});
