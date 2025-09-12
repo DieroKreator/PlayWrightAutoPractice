4dc70da534fc9fad0382eb6ef4df8330bb569d3a
DieroKreator <qtechwired@gmail.com>
2025-09-12 08:38:53 -0300

    Calendar validations

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -5,6 +5,7 @@ test("Calendar validations", async ({ page }) => {
     const monthNumber = "6";
     const date = "15";
     const year = "2027";
+    const expectedList = [monthNumber, date, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     await page.locator("react-date-picker__inputGroup").click();
@@ -13,6 +14,11 @@ test("Calendar validations", async ({ page }) => {
     await page.getByText(year).click();
     await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click();
     await page.locator("//abbr[text()='"+date+"']").click();
+    
+    const inputs = await page.locator(".react-date-picker__inputGroup input");
 
-
+    for(let i=0; i<expectedList.length; i++) {
+        const value = inputs.nth(i).inputValue();
+        expect(value).toBe(expectedList[i]);
+    }
 });
