3de6b349a4d49ef5cf177eba4c6e9ef6f766e57e
DieroKreator <qtechwired@gmail.com>
2025-09-16 17:39:10 -0300

    Added security test "you are not authorize"

---
+++ tests/network- test-2.spec.js
@@ -0,0 +1,18 @@
+const { test } = require('@playwright/test');
+
+
+test.only('Security test request intercept', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill("pemo@gmail.com");
+    await page.locator("#userPassword").fill("Iamking@000");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+
+    await page.locator("button[routerlink*='myorders']").click();
+    //login and reach orders page
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+        async route => route.continue({ url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=621661f884b053f6765465b6' }));
+    await page.locator("button:has-text('View')").first().click();
+});
