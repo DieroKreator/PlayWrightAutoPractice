a8171be3022e61a0fb325fa380909259bbaeb29d
DieroKreator <qtechwired@gmail.com>
2025-10-05 12:29:54 -0300

    Refactor on test

---
+++ tests/ClientAppPO.spec.js
@@ -0,0 +1,77 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pageObjects/LoginPage');
+
+test('Client App login', async ({ page }) => {
+    const username = "anshika@gmail.com";
+    const password = "Iamking@000";
+    const productName = "ZARA COAT 3";
+    const products = page.locator(".card-body");
+    const loginPage = new LoginPage(page);
+    await loginPage.goTo();
+    await loginPage.validLogin(username, password);
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+    const count = await products.count();
+    for (let i = 0; i < count; i++) {
+        if (await products.nth(i).locator("b").textContent() == productName) {
+            //add to cart
+            await products.nth(i).locator("button:has-text('Add To Cart')").click();
+            break;
+        }
+    }
+
+    //Zara Coat 3
+    await page.locator("[routerlink*='cart']").click();
+    page.locator("ul.cartWrap li.items").first().waitFor();
+
+    const bool = page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("text=Checkout").click();
+
+    // await page.pause();
+    /* TO CHECK */
+    // const cvvTxtBox = await page.locator('input[class="input txt"]').first().waitFor();
+    // await cvvTxtBox.fill("123");
+
+    //Identify edit box and enter one by one
+    await page.locator("input[placeholder*='Select Country']").pressSequentially('ind', { delay: 150 });
+    const dropdown = page.locator(".ta-results").first();
+    await dropdown.waitFor();
+
+    //Iterate on all the options and select India
+    const optionsCount = await dropdown.locator("button").count();
+    for (let i = 0; i < optionsCount; ++i) {
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if (text === " India") {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
+    await page.locator(".action__submit").click();
+
+    await page.locator("tbody").nth(3).waitFor();
+    expect(page.locator(".hero-primary").first()).toHaveText(" Thankyou for the order. ");
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+
+    //Dinamically find an order
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+
+    const rows = await page.locator("tbody tr");
+
+    for (let i = 0; i < await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+})
