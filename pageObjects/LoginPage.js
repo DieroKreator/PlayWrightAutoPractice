a119b8d4ed179014f6488d443856d6165c20a89a
DieroKreator <qtechwired@gmail.com>
2025-10-05 12:30:28 -0300

    added login page

---
+++ pageObjects/LoginPage.js
@@ -0,0 +1,20 @@
+class LoginPage {
+    constructor(page) {
+        this.page = page;
+        this.signInButton = page.locator("[value='Login']");
+        this.emailField = page.locator("#userEmail");
+        this.passwordField = page.locator("#userPassword");
+    }
+
+    async goTo() {
+        await this.page.goto("https://rahulshettyacademy.com/client");
+    }
+
+    async validLogin(username, password) {
+        await this.emailField.type(username);
+        await this.passwordField.type(password);
+        await this.signInButton.click();
+    }
+
+}
+module.exports = { LoginPage };
