72e64d4ccc003a4a917b7fdd4d3fff5a1446adb5
DieroKreator <qtechwired@gmail.com>
2025-10-05 14:50:21 -0300

    created dashboard page

---
+++ pageObjects/DashboardPage.js
@@ -0,0 +1,26 @@
+class DashboardPage {
+    constructor(page) {
+        this.products = page.locator(".card-body");
+        this.produtcsText = page.locator(".card-body b");
+        this.cart = page.locator("[routerlink*='cart']");
+    }
+
+    async searchProduct(productName) {
+
+        const titles = await this.produtcsText.allTextContents();
+        console.log(titles);
+        const count = await this.products.count();
+        for (let i = 0; i < count; i++) {
+            if (await this.products.nth(i).locator("b").textContent() == productName) {
+                //add to cart
+                await this.products.nth(i).locator("button:has-text('Add To Cart')").click();
+                break;
+            }
+        }
+    }
+
+    async navigateToCart() {
+        await this.cart.click();
+    }
+}
+module.exports = { DashboardPage };
