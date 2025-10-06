b25970189130471f9602b10f6732e9973ee51604
DieroKreator <qtechwired@gmail.com>
2025-10-06 17:18:53 -0300

    Refactor Client App login test to iterate over dataset from JSON file

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -4,33 +4,32 @@ const { POManager } = require('../pageObjects/POManager');
 const dataset = require('../utils/placeOrderTestData.json');
 
 for (const data of dataset) {
-    console.log(data);
+    test(`Client App login for ${data.productName}`, async ({ page }) => {
+        const poManager = new POManager(page);
+
+        /*Data is coming from json file */
+
+        const loginPage = poManager.getLoginPage();
+        await loginPage.goTo();
+        await loginPage.validLogin(data.username, data.password);
+
+        const dashboardPage = poManager.getDashboardPage();
+        await dashboardPage.searchProduct(data.productName);
+        await dashboardPage.navigateToCart();
+
+        const cartPage = poManager.getCartPage();
+        await cartPage.verifyProductIsDisplayed(data.productName);
+        await cartPage.checkout();
+
+        const orderReviewPage = poManager.getOrderReviewPage();
+        await orderReviewPage.searchCountryAndSelect("ind", "India");
+        const orderId = await orderReviewPage.submitAndGetOrderId();
+        console.log(orderId);
+
+        await dashboardPage.navigateToOrders();
+
+        const orderHistoryPage = poManager.getOrderHistoryPage();
+        await orderHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
+    })
 }
-test('Client App login', async ({ page }) => {
-    const poManager = new POManager(page);
-
-    /*Data is coming from json file */
-
-    const loginPage = poManager.getLoginPage();
-    await loginPage.goTo();
-    await loginPage.validLogin(dataset.username, dataset.password);
-
-    const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.searchProduct(dataset.productName);
-    await dashboardPage.navigateToCart();
-
-    const cartPage = poManager.getCartPage();
-    await cartPage.verifyProductIsDisplayed(dataset.productName);
-    await cartPage.checkout();
-
-    const orderReviewPage = poManager.getOrderReviewPage();
-    await orderReviewPage.searchCountryAndSelect("ind", "India");
-    const orderId = await orderReviewPage.submitAndGetOrderId();
-    console.log(orderId);
-
-    await dashboardPage.navigateToOrders();
-    
-    const orderHistoryPage = poManager.getOrderHistoryPage();
-    await orderHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
-})
