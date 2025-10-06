0989039d33fcd5929f5a7c02dda5131aaaa65f1c
DieroKreator <qtechwired@gmail.com>
2025-10-05 21:10:44 -0300

    Corrections and missing statements

--- pageObjects/CartPage.js
+++ pageObjects/CartPage.js
@@ -5,6 +5,7 @@ class CartPage {
     constructor(page) {
         this.page = page;
         this.cartProducts = page.locator("ul.cartWrap li.items").first();
+        this.productsText = page.locator(".card-body b");
         this.cart = page.locator("[routerlink*='cart']");
         this.orders = page.locator("button[routerlink*='myorders']");
         this.checkout = page.locator("text=Checkout");
